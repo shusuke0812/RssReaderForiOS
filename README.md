b7a0a8c1050fe48ea22737c5b91d490fff1418f3
shusuke0812 <shu.ota0812@gmail.com>
2020-08-04 15:36:11 +0900

    [add]初期コミット
    ・プロジェクトファイル作成
    ・READMEファイル作成

---
+++ README.md
@@ -0,0 +1,14 @@
+Noteの記事をRSSで取得して表示するアプリ
+====
+　
+## 開発環境
+- Xcode version 11.6
+- pod version 1.9.1
+- MacOS Catalina version 10.15.4
+
+## 概要
+### UI  
+
+## 参考文献
+- 
+
---
+++ RssReaderForiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RssReaderForiOS
+//
+//  Created by Shusuke Ota on 2020/8/4.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
