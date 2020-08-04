f7df6dfc8c56d6e8f85beef1cbb3b86bda61ce9c
shusuke0812 <shu.ota0812@gmail.com>
2020-08-04 15:54:12 +0900

    [add]tableViewCellのファイルを追加
    ・TableViewCellディレクトリを追加
    ・UITableViewCellファイル、xibファイルを追加

---
+++ RssReaderForiOS/TableViewCell/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  RssReaderForiOS
+//
+//  Created by Shusuke Ota on 2020/8/4.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
