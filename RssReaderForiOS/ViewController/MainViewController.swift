f8aae78701e2ffd0c6c98428581256cfe9ec1252
shusuke0812 <shu.ota0812@gmail.com>
2020-08-18 23:40:41 +0900

    [fix]tableViewにリフレッシュ機能を追加
    ・sceneDelegateを削除（swiftファイル、info.plistのmanifest）、AppDelegate修正
    ・tableViewにリフレッシュコントロールを設定
    ・ViewControllerとtableViewをIBOutlet接続

--- RssReaderForiOS/ViewController/MainViewController.swift
+++ RssReaderForiOS/ViewController/MainViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let refreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(self.refresh(sender:)), for: .valueChanged)
+    }
+    
+    private func initRefreshControl() {
+        let refresh: UIRefreshControl = UIRefreshControl()
+        refresh.addTarget(self, action: #selector(self.refresh(sender:)), for: .valueChanged)
+        self.tableView.refreshControl = refresh
+    }
+    
+    @objc private func refresh(sender: UIRefreshControl) {
+        print("DEBUG： リフレッシュが呼ばれました")
+        self.tableView.refreshControl?.endRefreshing()
     }
 
 
