905a16b199b6e1ae494e4ec5b77b968e91942297
shusuke0812 <shu.ota0812@gmail.com>
2020-08-27 15:47:10 +0900

    [add]rssで取得したデータを入れる構造体を追加

--- RssReaderForiOS/Entity/Articles.swift
+++ RssReaderForiOS/Entity/Articles.swift
@@ -8,6 +8,22 @@
 
 import UIKit
 
-class Articles: NSObject {
-
+struct Articles: Codable {
+    internal let status: String
+    internal let feed: Feed
+    internal let items: [Item]
+}
+struct Feed: Codable {
+    // TODO：imageを受け取れるようにする
+    internal let url: String
+    internal let title: String
+    internal let link: String
+    internal let description: String
+}
+struct Item: Codable {
+    // TODO：thumbnailを受け取れるようにする
+    internal let title: String
+    internal let link: String
+    internal let author: String
+    internal let description: String
 }
