ed923dc617db339ae762e86b14e26fb400f556af
shusuke0812 <shu.ota0812@gmail.com>
2020-08-30 16:56:04 +0900

    [add]プロジェクト内共通の定数データ格納用ファイルを追

---
+++ RssReaderForiOS/Static/CommonData.swift
@@ -0,0 +1,13 @@
+//
+//  CommonData.swift
+//  RssReaderForiOS
+//
+//  Created by Shusuke Ota on 2020/8/30.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import Foundation
+
+struct CommonData {
+    
+}
