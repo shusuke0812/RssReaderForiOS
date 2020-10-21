2b24df9f30b418d86f8e8de4254784c572069498
shusuke0812 <shu.ota0812@gmail.com>
2020-10-21 14:05:15 +0900

    [fix]TableViewCellのレイアウトを変更

--- RssReaderForiOS/View/TableViewCell/ArticleCell.swift
+++ RssReaderForiOS/View/TableViewCell/ArticleCell.swift
@@ -16,22 +16,18 @@ class ArticleCell: UITableViewCell {
     
     internal weak var delegate: ArticleCellDelegate?
     
-    @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var iconImageView: UIImageView!
-    @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var postDateLabel: UILabel!
     
     // セルがロードされた時に一度だけ実行する（初期化など）
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.setUI()
     }
-    
     // セルの選択状態を管理する
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-    
     // セルの再利用時に呼ばれる（例：キャッシュ削除に使う）
     override func prepareForReuse() {
         // 初期化処理等を書く
@@ -39,20 +35,11 @@ class ArticleCell: UITableViewCell {
 }
 
 extension ArticleCell {
-    /// サムネイルの初期化
-    private func initThumbnailImageView() {
-        // ImageViewのFrame内に画像を収める
-        self.thumbnailImageView.clipsToBounds = true
-        // ImageViewの角を取る
-        self.thumbnailImageView.layer.cornerRadius = 10
-        // 描画比率を設定する
-        self.thumbnailImageView.contentMode = .scaleAspectFill
-    }
     /// UIの設定
     /// - Parameters:
     ///   - article: note記事データ
-    internal func setUI(article: Article, index: Int) {
-        // 記事タイトル
-        self.titleLabel.text = article.items[index].title
+    private func setUI() {
+        self.titleLabel.text = "-"
+        self.postDateLabel.text = "-"
     }
 }
