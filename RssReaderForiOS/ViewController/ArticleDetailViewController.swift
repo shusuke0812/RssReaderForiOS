49d2752c789898ba93f304ad8a0d83333606b80f
shusuke0812 <shu.ota0812@gmail.com>
2020-10-23 10:33:00 +0900

    [add]note記事の詳細をwebViewで表示する処理を追加

--- RssReaderForiOS/ViewController/ArticleDetailViewController.swift
+++ RssReaderForiOS/ViewController/ArticleDetailViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import WebKit
 
 class ArticleDetailViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
     // 遷移元から記事情報を受け取るプロパティ
-    var article: Item?
+    var article: Item!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(self.article!)
+        self.setUI()
+        self.setWebView()
+    }
+}
+
+extension ArticleDetailViewController {
+    private func setUI() {
+        // ナビゲーションバーの設定
+        self.title = "note記事詳細"
+    }
+    private func setWebView() {
+        let url = URL(string: self.article.link)
+        let request = NSURLRequest(url: url!)
+        self.webView.load(request as URLRequest)
     }
 }
