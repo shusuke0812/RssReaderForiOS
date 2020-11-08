535e0e2567e276d965eb454b126878c4bdc144f9
shusuke0812 <shu.ota0812@gmail.com>
2020-11-08 15:13:27 +0900

    [add]webView読み込み時に表示するProgressViewを追加

--- RssReaderForiOS/Static/CommonData.swift
+++ RssReaderForiOS/Static/CommonData.swift
@@ -15,4 +15,9 @@ struct CommonData {
         // XML → JSONに変換する（rss2json.com）
         internal static let noteArticle: String = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnote.mu%2Ffromkk%2Frss"
     }
+    /// ProgressViewの状態
+    struct ProgressViewStatus {
+        internal static let isLoading: String = "loading"
+        internal static let estimatedProgress: String = "estimated"
+    }
 }
--- RssReaderForiOS/ViewController/ArticleDetailViewController.swift
+++ RssReaderForiOS/ViewController/ArticleDetailViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import WebKit
+import PKHUD
 
 class ArticleDetailViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
+    // ProgressView初期化
+    private var progressView = UIProgressView(progressViewStyle: .bar)
     // 遷移元から記事情報を受け取るプロパティ
     var article: Item!
     
@@ -19,6 +22,7 @@ class ArticleDetailViewController: UIViewController {
         super.viewDidLoad()
         self.setUI()
         self.setWebView()
+        self.setProgressView()
     }
 }
 
@@ -31,5 +35,32 @@ extension ArticleDetailViewController {
         let url = URL(string: self.article.link)
         let request = NSURLRequest(url: url!)
         self.webView.load(request as URLRequest)
+    }
+    private func setProgressView() {
+        guard let navigationBarHeight = self.navigationController?.navigationBar.frame.size.height else { return }
+        self.progressView = UIProgressView(frame: CGRect(x: 0.0, y: navigationBarHeight, width: self.view.frame.size.width, height: 0.0))
+        self.navigationController?.navigationBar.addSubview(progressView)
+        self.webView.addObserver(self, forKeyPath: CommonData.ProgressViewStatus.isLoading, options: .new, context: nil)
+        self.webView.addObserver(self, forKeyPath: CommonData.ProgressViewStatus.estimatedProgress, options: .new, context: nil)
+    }
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard let keyPath = keyPath else { return }
+        switch keyPath {
+            case CommonData.ProgressViewStatus.isLoading:
+                if self.webView.isLoading {
+                    self.progressView.alpha = 1.0
+                    self.progressView.setProgress(0.1, animated: true)
+                } else {
+                    UIView.animate(withDuration: 0.3, animations: {
+                        self.progressView.alpha = 0.0
+                    }, completion: { _ in
+                        self.progressView.setProgress(0.0, animated: false)
+                    })
+                }
+            case CommonData.ProgressViewStatus.estimatedProgress:
+                self.progressView.setProgress(Float(self.webView.estimatedProgress), animated: true)
+            default:
+                break
+        }
     }
 }
