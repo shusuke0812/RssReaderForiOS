4385c6436ec3d390fa766546c1e6247bb67a2e24
shusuke0812 <shu.ota0812@gmail.com>
2021-11-23 23:38:02 +0900

    add: XML parse Helperクラスを追加

--- RssReaderForiOS/Utility/Helper/XMLParseHelper.swift
+++ RssReaderForiOS/Utility/Helper/XMLParseHelper.swift
@@ -8,6 +8,53 @@
 
 import Foundation
 
-class XMLParser {
+class XMLParseHelper: NSObject {
     
+    private let parser: XMLParser?
+    private var checkElement: String?
+
+    // TODO: [String] ではなく [Item] を返すようにする
+    var onSuccess: (([String]) -> Void)?
+    var onError: ((Error) -> Void)?
+
+    private var items: [String]
+    
+    init(data: Data) {
+        self.parser = XMLParser(data: data)
+        self.items = []
+        super.init()
+        self.parser?.delegate = self
+    }
+    
+}
+
+// MARK: - XMLParser Delegate
+extension XMLParseHelper: XMLParserDelegate {
+    // Analysis
+    func parserDidStartDocument(_ parser: XMLParser) {
+    }
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        print("DEBUG: XML parse element -> ", elementName)
+        if elementName == "ERROR" {
+            parser.abortParsing()
+        }
+        checkElement = elementName
+    }
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if string != "\n" {
+            if checkElement == "title" {
+                items.append(string)
+            }
+        }
+    }
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        print("DEBUG: XML parse element -> ", elementName)
+    }
+    func parserDidEndDocument(_ parser: XMLParser) {
+        onSuccess?(items)
+    }
+    // Error
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        onError?(parseError)
+    }
 }
