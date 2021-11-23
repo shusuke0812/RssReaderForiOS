638667d5c596fc212d7955743587d8a072215c0a
shusuke0812 <shu.ota0812@gmail.com>
2021-11-23 23:23:39 +0900

    add: XMLをparseするHelperクラスのファイルを追加

---
+++ RssReaderForiOS/Utility/Helper/XMLParseHelper.swift
@@ -0,0 +1,13 @@
+//
+//  XMLParser.swift
+//  RssReaderForiOS
+//
+//  Created by Shusuke Ota on 2021/11/23.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import Foundation
+
+class XMLParser {
+    
+}
